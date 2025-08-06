6fcea86cdfa9aea325d5f2552f4d26f34d89a4ef
unknown <deyan.todorov21@gmail.com>
2025-08-06 18:38:53 +0300

    [dyad] Replaced text with SVG logo - wrote 2 file(s)

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,5 +1,5 @@
 export const Logo = () => (
   <div className="flex justify-center py-4">
-    <h1 className="text-5xl font-bold tracking-tighter">MyGuy</h1>
+    <img src="/logo.svg" alt="MyGuy Logo" className="h-24" />
   </div>
 );
