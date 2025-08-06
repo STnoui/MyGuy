c8288e717641553f46da0e26b9905d5192c1c46b
unknown <deyan.todorov21@gmail.com>
2025-08-06 20:20:09 +0300

    [dyad] Permanently fixing all icon, card, and animation bugs - wrote 4 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -11,11 +11,8 @@ import Test from "./pages/Test";
 import { ThemeProvider } from "./components/ThemeProvider";
 import { I18nProvider } from "./contexts/I18nProvider";
 import { CallToAction } from "./components/CallToAction";
-import { AnimatePresence, motion } from "framer-motion";
-import { useI18n } from "./hooks/use-i18n";
 import { MobileNav } from "./components/MobileNav";
 import { SettingsMenu } from "./components/SettingsMenu";
-import { useTheme } from "next-themes";
 
 const queryClient = new QueryClient();
 
@@ -37,34 +34,20 @@ const App = () => (
 
 const AppContent = () => {
   const location = useLocation();
-  const { language } = useI18n();
-  const { theme } = useTheme();
-
   const showCallToAction = location.pathname === "/";
 
   return (
     <div className="flex flex-col h-screen bg-background">
       <MobileNav />
       <SettingsMenu />
-      <main className="flex-1">
-        <AnimatePresence mode="wait">
-          <motion.div
-            key={location.pathname + language + theme}
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            transition={{ duration: 0.4 }}
-            className="h-full"
-          >
-            <Routes location={location}>
-              <Route path="/" element={<Index />} />
-              <Route path="/contact" element={<Contact />} />
-              <Route path="/about" element={<About />} />
-              <Route path="/test" element={<Test />} />
-              <Route path="*" element={<NotFound />} />
-            </Routes>
-          </motion.div>
-        </AnimatePresence>
+      <main className="flex-1 h-full">
+        <Routes location={location}>
+          <Route path="/" element={<Index />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/test" element={<Test />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </main>
       {showCallToAction && <CallToAction />}
     </div>
